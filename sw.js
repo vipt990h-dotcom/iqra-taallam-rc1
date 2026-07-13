0c5bf91f8e08189ecca99517c6ec2ebdf989550e
vipt990h-dotcom <vipt990h@gmail.com>
2026-07-13 11:29:30 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'iqra-taallam-rc1-2';
+const CORE = [
+  './', './index.html', './manifest.json', './icon.svg', './icon-192.png', './icon-512.png',
+  ...Array.from({length: 28}, (_, i) => `./${String(i + 1).padStart(2, '0')}.mp3`)
+];
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE)));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match(event.request).then(r => r || caches.match('./index.html')))
+  );
+});
